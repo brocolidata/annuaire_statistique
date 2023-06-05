2fc7671e44bd6d2cc54a13b9e3460ec011c3a5a2
amirbtb <76230761+amirbtb@users.noreply.github.com>
2023-06-05 16:37:07 +0000

    fix: missing base_year
    - hardcoded base_year with a case

--- lab/dbt/dataplatform/models/ods/indice_des_prix/ipp.sql
+++ lab/dbt/dataplatform/models/ods/indice_des_prix/ipp.sql
@@ -30,7 +30,12 @@ ipp as (
         libelle_fr,
         libelle_ar,
         year::integer as year,
-        value
+        value,
+        {# TODO: extract base_year from source instead of hardcoding it #}
+        case
+            when year::integer < 2020 then 2010
+            else 2018
+        end as base_year
     from pivoted_ipp
 )
 
