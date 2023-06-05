18944287c6aa9fbcfd78229d639797bd5df9b725
amirbtb <76230761+amirbtb@users.noreply.github.com>
2023-06-05 16:40:54 +0000

    feat: export ipp in ods to csv

--- lab/dbt/dataplatform/models/ods/indice_des_prix/ipp.sql
+++ lab/dbt/dataplatform/models/ods/indice_des_prix/ipp.sql
@@ -2,7 +2,7 @@
 {{ 
     config(
         materialized='external',
-        location="{{ env_var('DWH_DATA') }}/{{ model.config.database }}/{{ model.config.group }}/{{ model.name }}.parquet"
+        location="{{ env_var('DWH_DATA') }}/{{ model.config.database }}/{{ model.config.group }}/{{ model.name }}.csv"
     ) 
 }}
 
