553a4d1b8e30cf62dfc24a8856b8c1fb484f6daf
amirbtb <76230761+amirbtb@users.noreply.github.com>
2023-06-07 15:43:48 +0000

    feat: add IPP v2
    - extract ipp v2
    - transform and merge all versions of IPP
    - improve IPP doc

--- lab/dbt/dataplatform/models/ods/indice_des_prix/ipp.sql
+++ lab/dbt/dataplatform/models/ods/indice_des_prix/ipp.sql
@@ -6,7 +6,7 @@
     ) 
 }}
 
-with ipp_v1 as (
+with ipp_stg_v1 as (
     select
         libelle_fr,
         libelle_ar,
@@ -17,26 +17,58 @@ with ipp_v1 as (
     from {{ ref("ipp_v1_stg") }}
 ),
 
-pivoted_ipp as (
-    unpivot ipp_v1
+ipp_stg_v2 as (
+    select
+        libelle_fr,
+        libelle_ar,
+        "2021",
+        "2020",
+        "2019"
+    from {{ ref("ipp_v2_stg") }}
+),
+
+pivoted_ipp_v1 as (
+    unpivot ipp_stg_v1
     on columns(* exclude (libelle_fr, libelle_ar))
     into
         name year
         value value
 ),
 
-ipp as (
+pivoted_ipp_v2 as (
+    unpivot ipp_stg_v2
+    on columns(* exclude (libelle_fr, libelle_ar))
+    into
+        name year
+        value value
+),
+
+ipp_v1 as (
     select
         libelle_fr,
         libelle_ar,
         year::integer as year,
         value,
         {# TODO: extract base_year from source instead of hardcoding it #}
-        case
-            when year::integer < 2020 then 2010
-            else 2018
-        end as base_year
-    from pivoted_ipp
+        2010 as base_year
+    from pivoted_ipp_v1
+),
+
+ipp_v2 as (
+    select
+        libelle_fr,
+        libelle_ar,
+        year::integer as year,
+        value,
+        {# TODO: extract base_year from source instead of hardcoding it #}
+        2018 as base_year
+    from pivoted_ipp_v2
+),
+
+unioned_ipp as (
+    select * from ipp_v1
+    union
+    select * from ipp_v2
 )
 
-select * from ipp
+select * from unioned_ipp
